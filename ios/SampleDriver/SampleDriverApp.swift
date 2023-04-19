ed1b371024c636653cf6bd59c35f5e6580ec3797
sinjikoro <shinnaga.dev@gmail.com>
2023-04-19 15:06:37 +0900

    1st

---
+++ ios/SampleDriver/SampleDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDriverApp.swift
+//  SampleDriver
+//
+//  Created by 長崎 晋士 on 2023/04/18.
+//
+
+import SwiftUI
+
+@main
+struct SampleDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
